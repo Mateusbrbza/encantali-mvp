40b32596b36a5944849799ef98158eb3d475c08b
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-02-14 22:02:33 -0300

    app config

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/globals.scss";
+
+//pages
 import Home from "./pages/home/Home";
+import ProductDetail from "./pages/product-detail/ProductDetail";
+
+//components
+import Sidebar from "./components/sidebar/Sidebar";
+import Product from "./components/product/Product";
+import Hero from "./components/hero/Hero";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import CartItem from "./components/cart-item/CartItem";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
-   {
+  {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/product",
+    element: <ProductDetail />,
   },
 ]);
 
