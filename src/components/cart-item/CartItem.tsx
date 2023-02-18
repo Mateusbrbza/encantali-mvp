c084910de7f0adee16135b066298ec6278d3e944
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-02-18 18:50:50 -0300

    chore: building the cartitem

--- src/components/cart-item/CartItem.tsx
+++ src/components/cart-item/CartItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CartItem = () => {
   return (
@@ -6,4 +6,4 @@ const CartItem = () => {
   )
 }
 
-export default CartItem
+export default CartItem;
