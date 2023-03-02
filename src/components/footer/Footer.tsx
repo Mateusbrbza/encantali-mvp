5646f88a60becee927ab33923e9fe4ac421cdeda
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-03-02 19:01:04 -0300

    feat: finished Footer, starting about page

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import {AiOutlineInstagram, AiOutlineFacebook} from 'react-icons/ai';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      {/* <Image
-      src={FooterLogo}
-      alt="encantali site logo"
-      className={styles.icon}
-      grid-area="one"
-      /> */}
       <a className={styles.icon} href='/'>Encantali</a>
       <span className={styles.text} grid-area="two">
         Email: encantali@gmail.com <br />
         WhatsApp: +55 48 99999-9999
       </span>
       <span className={styles["mateus-text"]} grid-area="three">
-        Feito por Mateus Barboza 2023.
+        Feito por Mateus Barboza. 2023.
       </span>
       <div className={styles["social-links"]} grid-area="four">
-        <Link to={'https://www.instagram.com/use.encantali/'}>
+        <a href='https://www.instagram.com/use.encantali/' 
+        target="_blank" rel="noreferrer"
+        >
           <AiOutlineInstagram />
-        </Link>
-        <Link to={'https://www.facebook.com/use.encantali'}>
+        </a>
+        <a href='https://www.facebook.com/use.encantali' 
+        target="_blank" rel="noreferrer"
+        >
           <AiOutlineFacebook />
-        </Link>
+        </a>
       </div>
     </footer>
   )
